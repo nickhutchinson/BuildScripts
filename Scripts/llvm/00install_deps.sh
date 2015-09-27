e756ab79db3ea4f1df4ba0faee43dda5ece9d541
Nicholas Hutchinson <nshutchinson@gmail.com>
2015-09-27 13:05:25 +0100

    Additional dependencies, fixes for building on CentOS 5

--- Scripts/llvm/00install_deps.sh
+++ Scripts/llvm/00install_deps.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+set -euo pipefail
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source "$HERE/_common.sh"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+yum install -y curl
+
+if is_ancient_redhat; then
+    get_url "https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm"
+    yum install --nogpgcheck -y epel-release-latest-5.noarch.rpm
+fi
+
 deps=(
+    # Build-essentials
+    automake
+    bison
+    bzip2
+    diffutils
+    flex
+    gcc
+    gcc-c++
+    make
+    patch
+    wget
+
     # GCC
     zlib-devel
-    glibc-devel.i686
+    glibc-devel      # RHEL 5
+    glibc-devel.i686 # >RHEL 5
 
     # LLVM
     libxml2-devel
     swig
     libedit-devel
-    doxygen # not required, but it looks like LLVM is searching for it
+
+    # unsure if this is required, but it looks like LLVM is searching for it.
+    doxygen
 
     # Python deps
     sqlite-devel
@@ -17,4 +48,18 @@ deps=(
     tk-devel
 )
 
-yum install -y "${deps[@]}"
+if is_ancient_redhat; then
+    get_url "http://mirror.centos.org/centos/5/os/x86_64/CentOS/xz-libs-4.999.9-0.3.beta.20091007git.el5.x86_64.rpm"
+    get_url "http://mirror.centos.org/centos/5/os/x86_64/CentOS/xz-4.999.9-0.3.beta.20091007git.el5.x86_64.rpm"
+
+    deps+=(
+        xz-libs-4.999.9-0.3.beta.20091007git.el5.x86_64.rpm
+        xz-4.999.9-0.3.beta.20091007git.el5.x86_64.rpm
+    )
+    yum install --nogpgcheck -y "${deps[@]}"
+else
+    deps+=(
+        xz
+    )
+    yum install -y "${deps[@]}"
+fi
--- Scripts/llvm/_common.sh
+++ Scripts/llvm/_common.sh
@@ -8,12 +8,23 @@ INSTALL_PREFIX=/opt/toolchains/$PACKAGE_NAME
 ROOT="$(pwd)"
 STAGING="$ROOT/staging-${PACKAGE_NAME}"
 
+is_ancient_redhat() {
+    test -f /etc/redhat-release && \
+        cat /etc/redhat-release | grep -q 'release 5\.[0-9]\+'
+}
+
+if is_ancient_redhat; then
+    nproc() { getconf _NPROCESSORS_ONLN; }
+fi
+
 get_url() {
     basename=$(basename "$1")
     if [[ -f "$basename" ]]; then
         return
     fi
-    run curl -C - -fL -o "$basename.part" "$@"
+    flags=(-L -C - -fl)
+    is_ancient_redhat && flags+=(-k)
+    run curl "${flags[@]}" -o "$basename.part" "$@"
     run mv "$basename.part" "$basename"
 }
 
@@ -24,5 +35,10 @@ run() {
 
 extract() {
     run mkdir -p "$2"
-    run tar xfv "$1" --strip-components=1 -C "$2"
+    if is_ancient_redhat && [[ ${1: -3} == ".xz" ]]; then
+        # CentOS 5's tar doesn't recognise xz.
+        run xz -dc "$1" | tar x --strip-components=1 -C "$2"
+    else
+        run tar xfv "$1" --strip-components=1 -C "$2"
+    fi
 }
